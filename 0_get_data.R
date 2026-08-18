a238bd30952747627cacfe45c0feac453cf9b434
Emily Oaks <emily.m.oaks@gmail.com>
2026-08-17 20:47:18 -0400

    Getting us started with a project and data download file :)

---
+++ 0_get_data.R
@@ -0,0 +1,8 @@
+# Load Libraries
+# You may need to install the proxy and Rcpp packages
+# in order for FAOSTAT to work
+# install.packages("proxy", repos = "https://cran.rproject.org/")
+# install.packages("Rcpp")
+
+library(tidyverse)
+library(FAOSTAT)
